f09976453251307267a7bfd426e71c922b17e866
Andrew Graham-Yooll <andrewgy8@gmail.com>
2025-01-31 11:14:59 +0100

    Move all personal config to settings

--- settings.py
+++ settings.py
@@ -7,4 +7,7 @@ params = {
     "author": "Andrew Graham-Yooll",
     "site_url": "https://andrew.grahamyooll.com",
     "current_year": datetime.datetime.now().year,
+    "github_url": "https://github.com/andrewgy8",
+    "linkedin_url": "https://www.linkedin.com/in/andrew-graham-yooll-%F0%9F%8F%84-623771141/",
+    "gtag_id": "G-Z73RLG2NVR"
 }
